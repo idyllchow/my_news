ca7209702ddf1c54035b99a08f172c1a196a9430
idyllchow <idyllzhou@gmail.com>
2017-06-23 14:09:47 +0800

    make it better

--- hello.py
+++ hello.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/home')
+def hello_world():
+    return 'this is home page'
+
 if __name__ == '__main__':
     app.run()
